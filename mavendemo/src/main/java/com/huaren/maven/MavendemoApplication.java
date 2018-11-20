a2852312c19126422bf1d9ac8b3a0565c7a24cc0
binbinyouli <binbinyouli@qq.com>
2018-11-20 16:49:06 +0800

    测试

---
+++ mavendemo/src/main/java/com/huaren/maven/MavendemoApplication.java
@@ -0,0 +1,12 @@
+package com.huaren.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavendemoApplication.class, args);
+	}
+}
